5727fd5e14e75df16a61410ff98311571a9c6107
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-07 12:42:16 -0600

    consolidates logic into money class

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -2,15 +2,25 @@ package guru.springframework;
 
 public abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency() {
+        return currency;
+    }
 
     public abstract Money times (int multiplier);
 
-    public static Dollar dollar(int amount){
-        return new Dollar(amount);
+    public static Money dollar(int amount){
+        return new Dollar(amount, "USD");
     }
 
-    public static Franc franc(int amount){
-        return new Franc(amount);
+    public static Money franc(int amount){
+        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object) {
