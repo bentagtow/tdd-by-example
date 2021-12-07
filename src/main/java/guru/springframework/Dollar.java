6ae66ed30be52b5772dacf43855616f0d8aa0e9a
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-07 09:56:27 -0600

    adds money converter

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount) {
+
+        this.amount = amount;
+
+    }
+
+    void times(int multiplier)
+    {
+        amount = amount*multiplier;
+
+    }
+}
