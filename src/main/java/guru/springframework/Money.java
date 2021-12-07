85659a84b500a6d7fad719f9cc7b10a86b95cb8b
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-07 11:46:20 -0600

    removes direct references to dollar obj

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,10 +1,22 @@
 package guru.springframework;
 
-public class Money {
-    int amount;
+public abstract class Money {
+    protected int amount;
+
+    public abstract Money times (int multiplier);
+
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount){
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
+
+
         return amount == money.amount
                 && getClass().equals(object.getClass());
     }
